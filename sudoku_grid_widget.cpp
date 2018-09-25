ae68178ed6d19820792be73fd642b24e4e43ad53
Emerentius <emerentius@arcor.de>
2018-09-25 03:36:57 +0200

    refactor grid widget constructor
    separate into several functions

--- sudoku_grid_widget.cpp
+++ sudoku_grid_widget.cpp
@@ -4,6 +4,12 @@
 const int MAJOR_LINE_SIZE = 6;
 const int MINOR_LINE_SIZE = 2;
 
+// quadratic
+const int sudoku_size = 900;
+const int x_off = 30;
+const int y_off = 30;
+const int cell_size = 96;
+
 auto line(QWidget *parent, QFrame::Shape shape) -> QFrame* {
     auto frame = new QFrame(parent);
     auto size_policy = QSizePolicy(QSizePolicy::Minimum, QSizePolicy::Maximum);
@@ -24,12 +30,6 @@ auto minor_line(QWidget* parent, QFrame::Shape shape) -> QFrame* {
 }
 
 SudokuGridWidget::SudokuGridWidget(QWidget *parent) : QFrame(parent) {
-    // quadratic
-    auto sudoku_size = 900;
-    auto x_off = 30;
-    auto y_off = 30;
-    auto cell_size = 96;
-
     {
         this->setGeometry(QRect(x_off, y_off, sudoku_size, sudoku_size));
         auto size_policy = QSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
@@ -44,13 +44,20 @@ SudokuGridWidget::SudokuGridWidget(QWidget *parent) : QFrame(parent) {
     }
 
     // create grid of lines
-    //auto line_off = sudoku_size / 3;
-
     // using QFrame borders for lines
     // may wanna switch to QLine
-    std::vector<QFrame*> major_hor_lines;
-    std::vector<QFrame*> major_ver_lines;
+    this->generate_major_lines();
+    this->generate_minor_lines();
 
+    this->generate_cells();
+
+    m_sudoku = sudoku_generate_unique();
+    this->set_clues();
+    this->compute_candidates();
+};
+
+
+auto SudokuGridWidget::generate_major_lines() -> void {
     for (int i = 1; i < 3; i++) {
         auto offset = i * (MAJOR_LINE_SIZE + 3 * cell_size + 2 * MINOR_LINE_SIZE);
         auto hframe = new QFrame(this);
@@ -58,30 +65,27 @@ SudokuGridWidget::SudokuGridWidget(QWidget *parent) : QFrame(parent) {
         hline->setGeometry(
             QRect(MAJOR_LINE_SIZE, offset, sudoku_size, MAJOR_LINE_SIZE)
         );
-        major_hor_lines.push_back(hline);
 
         auto vline = line(this, QFrame::VLine);
         vline->setGeometry(
             QRect(offset, MAJOR_LINE_SIZE, MAJOR_LINE_SIZE, sudoku_size)
         );
-        major_ver_lines.push_back(vline);
     }
+}
 
-    std::vector<QFrame*> minor_hor_lines;
-    std::vector<QFrame*> minor_ver_lines;
-
+auto SudokuGridWidget::generate_minor_lines() -> void {
     auto pos = 0;
     for (int i = 0; i < 9; i++ ) {
         pos += cell_size + (i % 3 == 0 ? MAJOR_LINE_SIZE : MINOR_LINE_SIZE);
         auto hline = minor_line(this, QFrame::HLine);
         hline->setGeometry(QRect(0, pos, sudoku_size, MINOR_LINE_SIZE));
-        minor_hor_lines.push_back(hline);
 
         auto vline = minor_line(this, QFrame::VLine);
         vline->setGeometry(QRect(pos, 0, MINOR_LINE_SIZE, sudoku_size));
-        minor_ver_lines.push_back(vline);
     }
+}
 
+auto SudokuGridWidget::generate_cells() -> void {
     std::vector<SudokuCellWidget*> cells;
     auto x_pos = 0;
 
@@ -103,14 +107,28 @@ SudokuGridWidget::SudokuGridWidget(QWidget *parent) : QFrame(parent) {
         x_pos += cell_size;
     }
 
-    Sudoku sudoku = sudoku_generate_unique();
-    auto solver = strategy_solver_new(sudoku);
-    auto cell_ptr = sudoku_as_ptr(&sudoku);
+    m_cells = cells;
+}
+
+// Only for the initial round!
+// It sets all currently known entries as clues
+auto SudokuGridWidget::set_clues() -> void {
+    auto cell_ptr = sudoku_as_ptr(&m_sudoku);
     for (int cell = 0; cell < 81; cell++) {
-        auto& c = cells[cell];
+        //auto& c = m_cells[cell];
         if (*cell_ptr != 0) {
-            c->set_clue(*cell_ptr);
-        } else {
+            m_cells[cell]->set_clue(*cell_ptr);
+        }
+        cell_ptr++;
+    }
+}
+
+auto SudokuGridWidget::compute_candidates() -> void {
+    auto solver = strategy_solver_new(m_sudoku);
+    auto cell_ptr = sudoku_as_ptr(&m_sudoku);
+    for (int cell = 0; cell < 81; cell++) {
+        if (*cell_ptr == 0) {
+            auto& c = m_cells[cell];
             auto candidates = strategy_solver_cell_candidates(solver, cell);
             for (int dig = 1; dig < 10; dig++) {
                 if (candidates & 1) {
@@ -121,4 +139,4 @@ SudokuGridWidget::SudokuGridWidget(QWidget *parent) : QFrame(parent) {
         }
         cell_ptr++;
     }
-};
+}
--- sudoku_grid_widget.h
+++ sudoku_grid_widget.h
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "sudoku_cell_widget.h"
+#include "sudoku_ffi/src/sudoku_ffi/sudoku.h"
 #include <QFrame>
 
 class SudokuGridWidget final : public QFrame {
+    std::vector<SudokuCellWidget*> m_cells;
+    Sudoku m_sudoku;
+
     public:
         explicit SudokuGridWidget(QWidget *parent = 0);
+
+        auto sudoku() -> Sudoku;
+        auto generate_major_lines() -> void;
+        auto generate_minor_lines() -> void;
+        auto generate_cells() -> void;
+        auto set_clues() -> void;
+
+        auto compute_candidates() -> void;
 };
